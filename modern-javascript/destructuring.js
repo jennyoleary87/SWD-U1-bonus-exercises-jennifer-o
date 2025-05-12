e5167b3aee9ce5f86fdf9808013b2f8105b37336
jennyo <jennifer.oleary.10@gmail.com>
2025-05-12 13:47:25 -0500

    destructuring group work

--- modern-javascript/destructuring.js
+++ modern-javascript/destructuring.js
@@ -15,7 +15,7 @@ let line = phoneNumberParts[2];
 
 // There is a better syntax for this
 // Notice the use of the let keyword
-let [ areaCode, exchange, lineNumber ] = phoneNumberParts;
+let [areaCode, exchange, lineNumber] = phoneNumberParts;
 
 // Essentially you are extracting the elements and assigning new variable names for each of them
 
@@ -49,21 +49,29 @@ console.log(website);
 // YOUR TURN!
 
 // TODO: Create an array with several elements
-
+let arrFruits = ["Apple", "Orange", "Banana", "Mango", "Grapes"];
 
 // TODO: Destructure the array and assign variables
 // to each element
-
+let [firstPlace, secondPlace, thirdPlace, fourthPlace, fifthPlace] = arrFruits;
 
 // TODO: Print the values of the new variables
-
+console.log(`FirstPlace - ${firstPlace}, SecondPlace - ${secondPlace}, ThirdPlace - ${thirdPlace}
+    FourthPlace - ${fourthPlace}, FifthPlace - ${fifthPlace}`);
 
 // TODO: Create an object with several key/value pairs
-
+let movies = {
+    title: "Starwars",
+    year: 1977,
+    genre: "Sci-fi"
+};
 
 // TODO: Destructure the object and assign variables
 // to each property
-
+let { title, year, genre } = movies;
 
 // TODO: Print the values of the new variables
+console.log(title);
+console.log(year);
+console.log(genre);
 
